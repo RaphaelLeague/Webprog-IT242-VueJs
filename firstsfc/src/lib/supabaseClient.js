7474c602c46ca480a79f93d0386816642091ddce
Raphael Gabriel League <rsleague@student.apc.edu.ph>
2026-01-28 13:53:23 +0000

    added links

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const supabaseUrl = import.meta.env.https://avjulztmjwjfivabmmuy.supabase.co
+const supabasePublishableKey = import.meta.env.sb_publishable_EvJR7ZNfG20fjFxv-i66ig_a3rtrHFD
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
