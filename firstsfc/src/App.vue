7437351458d39d57f36e708a7c5f47a3c13cafa1
Raphael Gabriel League <rsleague@student.apc.edu.ph>
2026-01-28 13:55:29 +0000

    testfix

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
+import { supabase } from './lib/supabaseClient.js'
 const instruments = ref([])
 async function getInstruments() {
   const { data } = await supabase.from('instruments').select()
--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.https://avjulztmjwjfivabmmuy.supabase.co
-const supabasePublishableKey = import.meta.env.sb_publishable_EvJR7ZNfG20fjFxv-i66ig_a3rtrHFD
+const supabaseUrl = "https://avjulztmjwjfivabmmuy.supabase.co"
+const supabasePublishableKey = "sb_publishable_EvJR7ZNfG20fjFxv-i66ig_a3rtrHFD"
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
